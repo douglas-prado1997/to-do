afbf70273ea1c055195efea49b27cac388fcd39c
Douglas Prado <douglas.prado@forlogic.net>
2025-03-21 10:33:38 -0300

    Subindo aplicação

---
+++ api/Controllers/TasksController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace to_do_list.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TasksController> _logger;
+
+        public TasksController(ILogger<TasksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTasks")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
