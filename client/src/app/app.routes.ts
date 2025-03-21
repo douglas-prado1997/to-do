2455525af02691206ef6d528f2e51aba70d4910e
Douglas Prado <douglas.prado@forlogic.net>
2025-03-21 11:44:50 -0300

    Criando componente de login

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Ajuste o caminho conforme necessário
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, 
+
+];
