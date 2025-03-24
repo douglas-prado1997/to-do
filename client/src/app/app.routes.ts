aa93d489cff36a2fce98315be1316f86cd9472c2
Douglas Prado <douglas.prado@forlogic.net>
2025-03-24 16:57:50 -0300

    ajuste

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component'; // Ajuste o caminho conforme necessário
+import { TasksComponent } from './tasks/tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, 
+  { path: 'login', component: LoginComponent },
+  { path: 'tasks', component: TasksComponent },
 
 ];
